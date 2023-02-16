5be64049e2b2e32782d02d8ae270edbd9808a1cf
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-02-16 15:22:11 -0600

    Part3 nearly done

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -15,12 +21,27 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
 @RequestMapping("search")
 public class SearchController {
 
+    private static List<List> jobs = new ArrayList<>();
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs", jobs);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @PostMapping(value = "/results")
+    public String displaySearchResults(@RequestParam String searchTerm, @RequestParam String searchType, Model model) {
+        if (searchTerm.equals("all") || searchTerm.equals("")) {
+            jobs.add(JobData.findAll());
+        } else {
+            jobs.add(JobData.findByColumnAndValue(searchType, searchTerm));
+        }
+        System.out.println(jobs);
+        return "redirect:";
+    }
+
 
 }
