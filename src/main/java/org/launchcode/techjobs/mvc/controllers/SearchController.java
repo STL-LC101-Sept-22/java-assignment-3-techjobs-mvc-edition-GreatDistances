f1bf547d078c5a9953e878ead0c8297df4506a95
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-02-18 12:10:07 -0600

    20/20 tests passing, search/results nearly done

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -37,10 +37,6 @@ public class SearchController {
     @PostMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
-        // TODO ?? how do I fix redirect (which is functional) to return results at localhost:8080/search/results ?
-        // TODO ?? do i need to add some kind of model.AddAttribute here?
-        // TODO once this routing issue is solved, most tests in TestTaskFour will likely pass.
-
         if (searchTerm.equals("all") || searchTerm.equals("")) {
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
